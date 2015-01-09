bb05aa3f9d0aff17b463212d15331cc10146949c
Jesmine <damilola.omonori@andela.co>
2015-01-09 09:20:54 -0800

    added sum functionalities on index.html

--- app/js/custom/app.js
+++ app/js/custom/app.js
@@ -4,8 +4,16 @@ var wordBot = angular.module('wordBot', []);
       '$http', 
       function ($scope, $http){
 
-    var url = 'https://wordrobot.herokuapp.com/api/words',
-        $http.get(url).success( function (words){
-          console.log(words);
+    var url = 'https://wordrobot.herokuapp.com/api/words';
+    $scope.tab = true;
+    $scope.tocall = function () {
+      
+    }
+    $scope.words = [];
+        $http.get(url).success( function (data){
+          $scope.words=data;
+          console.log($scope.words);
+        }).error(function (data){
+            console.log("error: "+ data);
         });
     }]);
